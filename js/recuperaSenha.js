c6007d87e649778d022ded68be71e6a7273708dc
Lenner Augusto <102566312+LennerAugusto@users.noreply.github.com>
2022-11-11 15:07:44 -0300

    Implementação das páginas auxiliares do rodape

---
+++ js/recuperaSenha.js
@@ -0,0 +1,9 @@
+//Recebe o email do usuário durante a recuperação de senha
+function pegaEmail(){
+    var emailUsuario = document.getElementById("txtEmail").value;
+}
+
+function mostraEmail(){
+    document.setElementById("emailUsuario").innerHTML;
+}
+
